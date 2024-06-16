32e2b279f56da21da31f2cf4d63a1c1c6cd0430e
Dominik Kolarsky <kolarskydominik@gmail.com>
2024-06-16 20:06:48 +0200

    update: css to tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,8 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
+  imports: [RouterOutlet],
 })
 export class AppComponent {
   title = 'carpiness';
