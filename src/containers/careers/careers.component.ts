e3ef2ad300ab57f4ee448fa2ad40f587d7f19266
Dominik Kolarsky <kolarskydominik@gmail.com>
2024-06-25 08:03:43 +0200

    add: lorem

--- src/containers/careers/careers.component.ts
+++ src/containers/careers/careers.component.ts
@@ -1,15 +1,19 @@
 import { CommonModule } from "@angular/common";
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { PageSectionComponent } from "../../components/page-section/page-section.component";
+import { PageContainerComponent } from "../../components/page-container/page-container.component";
 
 @Component({
-  selector: 'app-careers',
-  standalone: true,
-  imports: [
-    CommonModule,
-  ],
-  templateUrl: './careers.component.html',
-  styleUrl: './careers.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'app-careers',
+    standalone: true,
+    templateUrl: './careers.component.html',
+    styleUrl: './careers.component.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [
+        CommonModule,
+        PageSectionComponent,
+        PageContainerComponent
+    ]
 })
 export class CareersComponent {}
 
--- src/containers/contact/contact.component.ts
+++ src/containers/contact/contact.component.ts
@@ -1,15 +1,19 @@
 import { CommonModule } from "@angular/common";
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { PageSectionComponent } from "../../components/page-section/page-section.component";
+import { PageContainerComponent } from "../../components/page-container/page-container.component";
 
 @Component({
-  selector: 'app-contact',
-  standalone: true,
-  imports: [
-    CommonModule,
-  ],
-  templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'app-contact',
+    standalone: true,
+    templateUrl: './contact.component.html',
+    styleUrl: './contact.component.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [
+        CommonModule,
+        PageSectionComponent,
+        PageContainerComponent
+    ]
 })
 export class ContactComponent {}
 
